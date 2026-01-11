4125d1b4af43187dfceacf654d95b342ce75151c
PhuongDuy-PP <15520659@gm.uit.edu.vn>
2026-01-11 15:50:26 +0700

    Create CI

--- tests/getByRole.spec.ts
+++ tests/getByRole.spec.ts
@@ -80,4 +80,76 @@ test.describe("Test getByRole với HTML local", () => {
 
         await page.waitForTimeout(2000)
     })
+
+    test("Test radio", async({page}) => {
+        // <label>
+        //     <input 
+        //         type="radio" 
+        //         id="male" 
+        //         name="gender" 
+        //         value="male"
+        //         aria-label="Male gender option"
+        //     >
+        //     Nam
+        // </label>
+        const maleRadio = page.getByRole("radio", {name: "male"}).first()
+
+        await expect(maleRadio).toBeVisible()
+
+        maleRadio.check()
+        await expect(maleRadio).toBeChecked()
+    })
+
+    test("Test table", async({page}) => {
+        // <table role="table">
+        //         <thead>
+        //             <tr>
+        //                 <th scope="col">ID</th>
+        //                 <th scope="col">Name</th>
+        //                 <th scope="col">Email</th>
+        //                 <th scope="col">Role</th>
+        //             </tr>
+        //         </thead>
+        //         <tbody>
+        //             <tr>
+        //                 <td>1</td>
+        //                 <td>John Doe</td>
+        //                 <td>john@example.com</td>
+        //                 <td>Admin</td>
+        //             </tr>
+        //             <tr>
+        //                 <td>2</td>
+        //                 <td>Jane Smith</td>
+        //                 <td>jane@example.com</td>
+        //                 <td>User</td>
+        //             </tr>
+        //             <tr>
+        //                 <td>3</td>
+        //                 <td>Bob Johnson</td>
+        //                 <td>bob@example.com</td>
+        //                 <td>Manager</td>
+        //             </tr>
+        //         </tbody>
+        //     </table>
+
+        const table = page.getByRole("table")
+        await expect(table).toBeVisible()
+
+        // kiểm tra data trong table
+        const johnRow = table.getByRole("cell", {name: "John Doe"})
+        await expect(johnRow).toBeVisible()
+
+        // kiểm tra trong table có bao nhiêu data
+        const rows = table.getByRole("row")
+        let countRow = await rows.count()
+        await expect(countRow).toEqual(4) // 1 header + 3 data
+    })
+
+    test("Test link", async({page}) => {
+        const navigation = page.getByRole("navigation")
+        await expect(navigation).toBeVisible()
+
+        const homeLink = navigation.getByRole("link").filter({hasText: "Home"}).first()
+        await expect(homeLink).toBeVisible()
+    })
 })
